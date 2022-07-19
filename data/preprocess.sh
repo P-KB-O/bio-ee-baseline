914b44136df7f15f5d132187f458a8f36515bed1
eleveyuan <eleveyuan@gmail.com>
2022-07-19 19:43:03 +0800

    changed

--- data/preprocess.sh
+++ data/preprocess.sh
@@ -1,2 +1,25 @@
 # preprocess raw xml data
-python preprocess4xml.py --data GE09 --input_dir ./xml
+echo "------------------starting to process raw data-----------------"
+res=$(python preprocess4xml.py --data GE09 --input_dir ./xml)
+echo $res
+
+# get input info for downstream task
+res=$(echo $res | grep 'trigger' | sed 's/<.* \([0-9]*\) trigger.* \([0-9]*\) entity.* \([0-9]*\) dep.*/\1 \2 \3/g')
+triggers=$(echo $res | awk -F ' ' '{print $1}')  # trigger classes
+entity=$(echo $res | awk -F ' ' '{print $2}')  # entity classes
+dep=$(echo $res | awk -F ' ' '{print $3}')  # dep classes
+echo "----------------------------end--------------------------------"
+
+
+echo "-------------------starting to construct dict------------------"
+python data2inputs.py --dest_dir ./preprocessed --data GE09 --seq_len 125
+echo "----------------------------end--------------------------------"
+
+
+echo "-------------------starting to read embedding------------------"
+res=$(python read_embedding.py --dest_dir ./preprocessed --data GE09 --entity_classes $entity)
+echo $res
+
+res=$(echo $res | grep 'word')
+word_num=$(echo $res | awk -F ' ' '{print $3}')  # the number of words
+echo "----------------------------end--------------------------------"
